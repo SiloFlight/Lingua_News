d88c076e8fa8b48214b12fb79b436ff695d25334
Silo_Flight <legomaniacny13@gmail.com>
2024-12-28 20:19:50 -0500

    Created initial Article Body component.

---
+++ client/src/components/ArticleBody.js
@@ -0,0 +1,27 @@
+import Container from "react-bootstrap/Container";
+
+function GetArticleBody({translatedText,sourceText}){
+    //articleText is an array of paragraphs. Each paragraph is an array of sentences.
+    //sourceText is an accompanying array of paragraphs. The respective index in articleText corresponds to the respective source text.
+
+    //On Hover on a sentence in the article, it should display a text box with the source text.
+
+    const ArticleBody = translatedText.map((paragraph,index)=>{
+        var paraText = "";
+
+        for(var i = 0; i < paragraph.length; i++){
+            paraText += paragraph[i];
+        }
+
+        return (<p>
+            {paraText}
+        </p>)
+    })
+
+    return (
+    <Container>
+        {ArticleBody}
+    </Container>)
+}
+
+export default GetArticleBody;
